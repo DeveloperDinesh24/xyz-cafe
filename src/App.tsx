5e25eb0e49b521d3bdac6cd9a2e57325443b72c8
Dinesh <1442dinesh6singh5@gmail.com>
2026-02-11 12:40:19 +0530

    feat: create footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import MenuSection from './components/MenuSection';
@@ -13,6 +14,7 @@ export default function App() {
     <AboutSection />
     <MenuSection />
     <ContactSection />
+    <Footer />
     </>
   )
 }
