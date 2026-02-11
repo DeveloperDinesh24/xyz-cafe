59c6d88d9c04e3e8dd30cd2dea93e83ae9465fda
Dinesh <1442dinesh6singh5@gmail.com>
2026-02-11 12:27:44 +0530

    feat: create menusection.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import MenuSection from './components/MenuSection';
 
 export default function App() {
   return (
     <>
     <Header />
     <HeroSection />
+    <MenuSection />
     </>
   )
 }
