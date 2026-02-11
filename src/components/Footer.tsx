092bc7e16c5a09d5d78c21e78856184c316d84e0
Dinesh <1442dinesh6singh5@gmail.com>
2026-02-11 12:40:40 +0530

    feat: complete footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-  <></>
+    <footer className='bg-black py-8 border-t border-gray-900 text-center text-gray-500 text-sm'>
+      <p>&copy; 2024 XYZ CAFE & RESTRO AJMER. ALL RIGHTS RESERVED.</p>
+    </footer>
   )
 }
